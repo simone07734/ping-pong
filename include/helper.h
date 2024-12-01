6492d860386cb847a5fd609fd033c1af2a5dfa0d
simone07734 <simone.m.laney@gmail.com>
2024-11-30 23:12:16 -0800

    Ball movement and collision
    * ball moves, bounces off walls
    * ball bounces off paddle, with angle changing based on where on the
      paddle it hits
    * ball returns to middle after hitting walls behind paddles

--- include/helper.h
+++ include/helper.h
@@ -5,21 +5,6 @@
 #ifndef HELPER_H
 #define HELPER_H
 
-//Functionality - finds the greatest common divisor of two values
-//Parameter: Two long int's to find their GCD
-//Returns: GCD else 0
-unsigned long int findGCD(unsigned long int a, unsigned long int b)
-{
-    unsigned long int c;
-    while(1){
-        c = a % b;
-        if( c == 0 ) { return b; }
-        a = b;
-        b = c;
-    }
-    return 0;
-}
-
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
     return (b ? (x | (0x01 << k)) : (x & ~(0x01 << k)) );
     // Set bit to 1 Set bit to 0
@@ -45,4 +30,5 @@ long map(long x, long in_min, long in_max, long out_min, long out_max) {
     if (val < out_min) { val = out_min; }
     return val;
 }
+
 #endif /* HEPLER_H */
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "SPI_AVR.h"
 #include "timerISR.h"
 
-#define NUM_TASKS 3
+#define NUM_TASKS 4
 
 // Game constants
 const char PADDLE_WIDTH = 26;
@@ -16,6 +16,8 @@ const short OBJECT_COLOR = (0xFFFF);
 // Shared variables
 unsigned char player1Loc[4] = {10, 10 + PADDLE_DEPTH, 52, 52 + PADDLE_WIDTH}; // in order: xs, xe, ys, ye
 unsigned char player2Loc[4] = {119 - PADDLE_DEPTH, 119, 52, 52 + PADDLE_WIDTH}; 
+unsigned char ballLoc[4] = {62, 62 + BALL_DIAMETER, 62, 62 + BALL_DIAMETER}; // in order: xs, xe, ys, ye
+signed char ballVec[2] = {2,2}; // in order:x, y
 unsigned char screenClear = 0;
 
 // Task struct for concurrent synchSMs implmentations
@@ -27,10 +29,11 @@ typedef struct _task{
 } task;
 
 // Task periods and GCD
-const unsigned long GCD_PERIOD = 50;
+const unsigned long GCD_PERIOD = 25;
 const unsigned long START_GAME_PERIOD = 500;
-const unsigned long PLAYER1_PERIOD = 100;
-const unsigned long PLAYER2_PERIOD = 100;
+const unsigned long PLAYER1_PERIOD = 25;
+const unsigned long PLAYER2_PERIOD = 25;
+const unsigned long BALL_PERIOD = 25;
 
 task tasks[NUM_TASKS]; // task array
 
@@ -38,9 +41,14 @@ task tasks[NUM_TASKS]; // task array
 enum StartGame { SG_INIT, SG_PLAY };
 enum Player1 { P1_INIT, P1_MOVE };
 enum Player2 { P2_INIT, P2_MOVE };
+enum Ball { B_INIT, B_PLAY };
 int Tick_Start_Game(int);
 int Tick_Player1(int);
 int Tick_Player2(int);
+int Tick_Ball(int);
+
+// Helper function declaration
+int checkCollision(void);
 
 void TimerISR() {
     for ( unsigned int i = 0; i < NUM_TASKS; i++ ) { // Iterate through each task in the task array
@@ -80,6 +88,11 @@ int main() {
     tasks[i].period = PLAYER2_PERIOD;
     tasks[i].elapsedTime = tasks[i].period;
     tasks[i].TickFct = &Tick_Player2;
+    i++;
+    tasks[i].state = B_INIT;
+    tasks[i].period = BALL_PERIOD;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &Tick_Ball;
 
     TimerSet(GCD_PERIOD);
     TimerOn();
@@ -122,6 +135,7 @@ int Tick_Player1(int state) {
             break;
         default:
             state = P1_INIT;
+            break;
     }
     switch (state) { // Actions
         case P1_MOVE:
@@ -149,6 +163,7 @@ int Tick_Player2(int state) {
             break;
         default:
             state = P2_INIT;
+            break;
     }
     switch (state) { // Actions
         case P2_MOVE:
@@ -164,4 +179,116 @@ int Tick_Player2(int state) {
             break;
     }   
     return state;
+}
+
+int Tick_Ball(int state) {
+    static unsigned char newX;
+    static unsigned char newY;
+    switch (state) { // Transitions
+        case B_INIT:
+            if (screenClear) { state = B_PLAY; }
+            break;
+        case B_PLAY:
+            break;
+        default:
+            state = B_INIT;
+            break;
+    }
+    switch (state) { // Actions
+        case B_PLAY:
+
+            checkCollision();
+            displayBlock(ballLoc[0], ballLoc[1], ballLoc[2], ballLoc[3], BACKGROUND_COLOR); // clear previous paddle
+            
+            // TODO: move into helper function
+            newX = ballLoc[0] + ballVec[0]; // get new ball xs
+            newY = ballLoc[2] + ballVec[1]; // get new ball ys
+
+            if(newX <= 0) {newX = 0;}
+            if(newX >= 128) {newX = 128;}
+            if(newY <= 0) {newY = 0;}
+            if(newY >= 128) {newY = 128;}
+
+            ballLoc[0] = newX;
+            ballLoc[1] = newX + BALL_DIAMETER;
+            ballLoc[2] = newY;
+            ballLoc[3] = newY + BALL_DIAMETER;
+            
+            displayBlock(ballLoc[0], ballLoc[1], ballLoc[2], ballLoc[3], OBJECT_COLOR); // display paddle at new location
+            break;
+        default:
+            break;
+    }   
+    return state;
+}
+
+// Helper functions
+/* Checks whether the ball has collided with the walls or paddles, and changes it's vector accordingly.
+   Should be called before clearing the old ball from the display. 
+   Returns 1 when player 1 scores, 2 when player 2 scores, and 0 when nobody scores. */
+int checkCollision(void) {
+    // side walls
+    if (ballLoc[2] <= 2 || ballLoc[3] >= 127) {
+        ballVec[1] *= -1;
+    }
+
+    // paddle 1
+    if ((ballLoc[0] >= player1Loc[0] && ballLoc[0] <= player1Loc[1])) {
+        // adjust vector based on where on the paddle it hits
+        if (ballLoc[3] >= player1Loc[2] && ballLoc[3] <= player1Loc[2] + 8) {
+            ballVec[0] *= -1;
+            ballVec[1] += 1;
+        }
+        else if (ballLoc[3] >= player1Loc[2] + 8 && ballLoc[3] <= player1Loc[3] - 8) {
+            ballVec[0] *= -1;
+            ballVec[1] -= 1;
+        }
+        else if (ballLoc[3] >= player1Loc[3] - 8 && ballLoc[2] <= player1Loc[3]) {
+            ballVec[0] *= -1;
+            ballVec[1] += 1; 
+        }
+        else {}
+        if (ballVec[1] > 4) { ballVec[1] = 4; }
+        if (ballVec[1] < 1) { ballVec[1] = 1; }
+    }
+
+    // paddle 2
+    if (ballLoc[1] >= player2Loc[0] && ballLoc[1] <= player2Loc[1]) {
+        // adjust vector based on where on the paddle it hits
+        if (ballLoc[3] >= player2Loc[2] && ballLoc[3] <= player2Loc[2] + 8) {
+            ballVec[0] *= -1;
+            ballVec[1] += 1;
+        }
+        else if (ballLoc[3] >= player2Loc[2] + 8 && ballLoc[3] <= player2Loc[3] - 8) {
+            ballVec[0] *= -1;
+            ballVec[1] -= 1;
+        }
+        else if (ballLoc[3] >= player2Loc[3] - 8 && ballLoc[2] <= player2Loc[3]) {
+            ballVec[0] *= -1;
+            ballVec[1] += 1; 
+        }
+        else {}
+        if (ballVec[1] > 4) { ballVec[1] = 4; }
+        if (ballVec[1] < 1) { ballVec[1] = 1; }
+    }
+
+    // behind paddle 1
+    if (ballLoc[0] <= 4) {
+        ballLoc[0] = 50;
+        ballLoc[1] = 54;
+        ballLoc[2] = 50;
+        ballLoc[3] = 54;
+        return 2; // player 2 gets a point
+    }
+
+    // behind paddle 2
+    if (ballLoc[1] >= 125) {
+        ballLoc[0] = 50;
+        ballLoc[1] = 54;
+        ballLoc[2] = 50;
+        ballLoc[3] = 54;
+        return 1; // player 1 gets a point
+    }
+    
+    return 0; // no points awarded
 }
